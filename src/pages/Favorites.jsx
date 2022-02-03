7d06e257d2011aa0cfac41d5f7a47bbb92f54f49
Filipe <filipe.b.braga@gmail.com>
2022-02-03 10:18:33 -0300

    passasando nos requisitos 12 e 13

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,13 +1,114 @@
 import React from 'react';
 import Header from '../components/Header';
+import {
+  addSong,
+  getFavoriteSongs,
+  removeSong,
+} from '../services/favoriteSongsAPI';
 
 class Favorites extends React.Component {
+  constructor(props) {
+    super(props);
+    // const { match } = this.props;
+    // how to get params
+    // https://stackoverflow.com/questions/44318631/how-get-the-value-of-match-params-id-on-react-router
+    this.state = {
+      isLoadingMessageEnable: false,
+      favoriteSongs: [],
+      // id: match.params.id,
+      // album: [],
+      // albumInfos: {
+      //   title: '',
+      //   cover: '',
+      //   artist: '',
+      // },
+    };
+    this.addFavSongFunc = this.addFavSongFunc.bind(this);
+  }
+
+  componentDidMount() {
+    // this.getMusicsFromApi();
+    this.loadingLocalStorage();
+    getFavoriteSongs();
+  }
+
+  loadingLocalStorage() {
+    const array = JSON.parse(localStorage.getItem('favorite_songs'));
+    this.setState({
+      isLoadingMessageEnable: false,
+      favoriteSongs: array,
+    });
+  }
+
+  checkIfHasInFavorite(item) {
+    const { favoriteSongs } = this.state;
+    const resultado = favoriteSongs.some((ele) => ele.trackId === item.trackId);
+    return resultado;
+  }
+
+  addFavSongFunc(item) {
+    const array = JSON.parse(localStorage.getItem('favorite_songs'));
+    const check = array.some((ele) => ele.trackId === item.trackId);
+
+    if (check) {
+      this.setState({ isLoadingMessageEnable: true });
+      removeSong(item).then(() => {
+        this.loadingLocalStorage();
+      });
+    } else {
+      this.setState({ isLoadingMessageEnable: true });
+      addSong(item).then(() => {
+        this.loadingLocalStorage();
+      });
+    }
+  }
+
   render() {
+    const {
+      favoriteSongs,
+      // albumInfos: { title, artist  cover },
+      isLoadingMessageEnable,
+    } = this.state;
+
     return (
       <div data-testid="page-favorites">
         <Header />
 
         <h1>Favorites</h1>
+        {isLoadingMessageEnable ? (
+          <p>Carregando...</p>
+        ) : (
+          <ul>
+            {favoriteSongs.map((item) => (
+              <div key={ item.trackId }>
+                <li>{`${item.trackName}`}</li>
+                <audio
+                  data-testid="audio-component"
+                  src={ item.previewUrl }
+                  controls
+                >
+                  <track kind="captions" />
+                  O seu navegador não suporta o elemento
+                  <code>audio</code>
+                </audio>
+                <label htmlFor="Favorita">
+                  Favorita
+                  <input
+                    name="Favorita"
+                    id="Favorita"
+                    type="checkbox"
+                    data-testid={ `checkbox-music-${item.trackId}` }
+                    checked={ this.checkIfHasInFavorite(item) }
+                    onChange={ () => {
+                      this.addFavSongFunc(item);
+                    } }
+                    // onChange={ () => { this.checkIfHasInFavorite(item.trackId); } }
+                  />
+                </label>
+              </div>
+            ))}
+          </ul>
+        )}
       </div>
     );
   }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,56 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
 
 class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoadingMessageEnable: false,
+      infos: '',
+    };
+  }
+
+  componentDidMount() {
+    getUser();
+    this.setUserInState();
+  }
+
+  setUserInState() {
+    const infos = JSON.parse(localStorage.getItem('user'));
+    // console.log(infos);
+    this.setState({ infos });
+  }
+
   render() {
+    const {
+      isLoadingMessageEnable,
+      infos,
+    } = this.state;
+
     return (
       <div data-testid="page-profile">
         <Header />
 
         <h1>Profile</h1>
+
+        {isLoadingMessageEnable ? (
+          <p>Carregando...</p>
+        ) : (
+          <div>
+            <img
+              data-testid="profile-image"
+              alt="Profile"
+              src="url-to-image"
+            />
+            <h4>{infos.description}</h4>
+            <h4>{infos.email}</h4>
+            <h4>{infos.image}</h4>
+            <h4>{infos.name}</h4>
+            <Link to="/profile/edit">Editar perfil</Link>
+          </div>
+        )}
       </div>
     );
   }
